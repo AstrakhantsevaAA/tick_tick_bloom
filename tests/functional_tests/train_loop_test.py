178537687383a4b0486caa070a20ed1d9a610f78
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-04 10:35:53 +0000

    [test] add tests for train loop

---
+++ tests/functional_tests/train_loop_test.py
@@ -0,0 +1,50 @@
+import pytest
+from omegaconf import DictConfig, OmegaConf
+
+from src.train.classificator.train import Trainer
+
+
+class TestTrainLoop:
+    @pytest.fixture(scope="class")
+    def set_conf(self, data_dir, csv_path) -> DictConfig:
+        conf = OmegaConf.create(
+            {
+                "train": {
+                    "log_clearml": False,
+                    "epochs": 3,
+                    "task_name": "test",
+                    "model_save_path": "",
+                },
+                "dataloader": {
+                    "data_dir": data_dir,
+                    "csv_path": csv_path,
+                    "augmentations_intensity": 0.5,
+                    "test_size": 10,
+                    "batch_size": 2,
+                },
+                "net": {
+                    "resume_weights": "",
+                    "freeze_grads": False,
+                    "pretrained": False,
+                    "model_name": "resnet18",
+                },
+                "optimizer": {"optimizer_name": "adam", "lr": 0.0003},
+                "scheduler": {"scheduler": False, "t0": 1, "t_mult": 2},
+            }
+        )
+        return conf
+
+    def test_loss_decreasing(self, set_conf):
+        trainer = Trainer(set_conf)
+        loss1 = trainer.train_one_epoch(1)
+        loss2 = trainer.train_one_epoch(2)
+
+        assert loss1 > loss2
+
+    def test_deterministic(self, set_conf):
+        trainer1 = Trainer(set_conf)
+        loss1 = trainer1.train_model()
+        trainer2 = Trainer(set_conf)
+        loss2 = trainer2.train_model()
+
+        assert loss1 == loss2
