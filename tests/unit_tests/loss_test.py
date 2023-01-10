0437ddf6e425350b18ebaa7373e360880cea9289
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-10 13:32:10 +0000

    [test] move numpy phi and gamma to test script

--- tests/unit_tests/loss_test.py
+++ tests/unit_tests/loss_test.py
@@ -3,7 +3,18 @@ import numpy as np
 import torch
 from einops import asnumpy
 
-from src.train.classificator.loss import gamma, gamma_torch, phi, phi_torch
+from src.data_utils.transforms import gamma_torch, phi_torch
+
+
+def phi(x: np.ndarray) -> np.ndarray:
+    x = np.where(x < 1, 0.5, x)
+    x = np.where((1e1 <= x) & (x < 2e4), 0.5 * x, x)
+    x = np.where((2e4 <= x) & (x < 1e5), 1.125 * x - 12500, x)
+    return x
+
+
+def gamma(x: np.ndarray) -> np.ndarray:
+    return np.log10(phi(x) / 100).clip(1, 5)
 
 
 @pytest.fixture
@@ -11,10 +22,17 @@ def example():
     return [90000, 2, 0, 14000, 2000, 10000000]
 
 
-def test_gamma(example):
+@pytest.fixture
+def result():
+    return [2.9482, 1., 1., 1.8451, 1., 5.]
+
+
+def test_gamma(example, result):
+    print(gamma_torch(torch.tensor(example)))
     assert np.allclose(
         asnumpy(gamma_torch(torch.tensor(example))), gamma(np.array(example))
     )
+    assert np.allclose(asnumpy(gamma_torch(torch.tensor(example))), np.array(result))
 
 
 def test_phi(example):
