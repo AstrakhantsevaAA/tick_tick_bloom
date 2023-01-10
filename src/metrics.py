00d1d9a693a412de71103170edb5b83627d3134f
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-10 22:19:54 +0000

    [feature] run weighted_mse with dataframe

--- src/metrics.py
+++ src/metrics.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from sklearn.metrics import mean_squared_error
 import numpy as np
+from src.config import system_config
 
 
 def weighted_rmse(data: pd.DataFrame):
     region_scores = []
     for region in data.region.unique():
         sub = data[data.region == region]
-        region_rmse = mean_squared_error(sub.severity, sub.pred.values.astype(int), squared=False)
+        region_rmse = mean_squared_error(sub.severity, sub.pred_int.values, squared=False)
         print(f"RMSE for {region} (n={len(sub)}): {round(region_rmse, 4)}")
         region_scores.append(region_rmse)
 
@@ -15,3 +16,8 @@ def weighted_rmse(data: pd.DataFrame):
     print(f"Final score: {overall_rmse}")
 
     return overall_rmse
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(system_config.data_dir / "benchmark/output/prediction_validation.csv")
+    weighted_rmse(df)
