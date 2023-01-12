793640a862b721f8ad499342cdec0592fc7c80ef
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-12 12:07:27 +0000

    [test] add test for sampler

--- src/train/classificator/sampler.py
+++ src/train/classificator/sampler.py
@@ -1,45 +1,34 @@
-from pathlib import Path
-
+import pandas as pd
+from loguru import logger
 from torch.utils.data import WeightedRandomSampler
 
 from src.data_utils.dataset import AlgalDataset
 
 
-def calculate_weights(df):
+def calculate_weights(df: pd.DataFrame) -> dict:
     weights = dict(df.groupby("region")["uid"].agg("count").astype(float) ** -1)
     w_sum = sum(weights.values())
     weights = {k: 4 * v / w_sum for k, v in weights.items()}
-
+    logger.info(f"Weights for {df.loc[:, 'split'].values[0]}: {weights}")
     return weights
 
 
-def define_sampler(dataset):
+def define_sampler(dataset: AlgalDataset) -> WeightedRandomSampler:
     df_full = dataset.df_full
     df_train = df_full[df_full["split"] == "train"]
     df_val = df_full[df_full["split"] == "validation"]
+    df_test = df_full[df_full["split"] == "test"]
 
     weights_train = calculate_weights(df_train)
     weights_val = calculate_weights(df_val)
-    weights = {k: v / weights_val[k] for k, v in weights_train.items()}
+    weights_test = calculate_weights(df_test)
 
-    print(weights_train, weights_val, weights)
+    weights_adv = {k: v / weights_val[k] for k, v in weights_train.items()}
+    logger.info(f"Adversarial weights: {weights_adv}")
 
     sampler = WeightedRandomSampler(
-        [weights[x] for x in dataset.regions],
+        [weights_adv[x] for x in dataset.regions],
         len(dataset.regions),
     )
 
     return sampler
-
-
-if __name__ == "__main__":
-    dataset = AlgalDataset(
-        Path(
-            "/home/alenaastrakhantseva/PycharmProjects/tick_tick_bloom/data/benchmark/image_arrays"
-        ),
-        Path(
-            "/home/alenaastrakhantseva/PycharmProjects/tick_tick_bloom/data/splits/balanced_validation/dumb_split_full_df.csv"
-        ),
-        phase="train",
-    )
-    define_sampler(dataset)
