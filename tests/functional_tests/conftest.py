c37774c94d78c08dc240deb9b898586d93959bbc
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-18 17:37:47 +0000

    update split and data dir

--- tests/functional_tests/conftest.py
+++ tests/functional_tests/conftest.py
@@ -8,12 +8,12 @@ from src.data_utils.dataset import AlgalDataset
 
 @pytest.fixture(scope="class")
 def data_dir() -> Path:
-    return system_config.data_dir / "benchmark/image_arrays"
+    return system_config.data_dir / "arrays/more_arrays_fixed"
 
 
 @pytest.fixture(scope="class")
 def csv_path() -> Path:
-    return system_config.data_dir / "splits/balanced_validation/dumb_split_full_df.csv"
+    return system_config.data_dir / "splits/downloaded.csv"
 
 
 @pytest.fixture()
