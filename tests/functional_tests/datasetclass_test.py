8b018539aa14540aa19449a0787254144736fc05
AstrakhantsevaAA <astra92293@gmail.com>
2023-01-26 11:05:02 +0000

    check hhr

--- tests/functional_tests/datasetclass_test.py
+++ tests/functional_tests/datasetclass_test.py
@@ -1,12 +1,15 @@
-from src.config import net_config
+from src.config import data_config, net_config
 
 
 def test_datasetclass(dataset):
-    sample = dataset[14]
+    sample = dataset[199]
 
     print(sample["image"].shape)
     print(sample["label"])
     print(sample["label_origin"])
+    print(sample["hrrr"])
+    print(sample)
 
     assert len(sample["image"].shape) == 3
     assert sample["image"].shape[0] == net_config.in_channels
+    assert len(sample["hrrr"]) == len(data_config.best_features)
